a70f3289e79a9b7b30a78b3caeac8ce0886b9bc7
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-05-26 17:43:11 -0500

    fixed small issue with store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import collectionReducer from './features/collection/collectionSlice';
+import userDataReducer from './features/userdata/userDataSlice'
 
 export const store = configureStore({
     reducer: {
-        collection: collectionReducer
+        collection: collectionReducer,
+        userdata: userDataReducer,
 
     }
 })
